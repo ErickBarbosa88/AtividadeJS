e44f3c1306ef52a87522fa10bf6de4af35a4fe98
ErickBarbosa88 <barbosaerick88@gmail.com>
2022-04-05 18:43:57 -0300

    default

---
+++ Ex4/script.js
@@ -0,0 +1,12 @@
+//const inputNome = document.getElementById("inputNome");
+const btInserir = document.getElementById("btInserir");
+const inputNome = document. getElementById("inputNome");
+const nome = document.getElementById("inputNome")
+function inserir() {
+    const nome = (inputNome.value);
+    alert("Olá" + nome)
+    console.log(nome)
+}
+
+
+//btInserir.onclick = inserir;
